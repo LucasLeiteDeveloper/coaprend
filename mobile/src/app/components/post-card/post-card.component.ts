0c30e20defc4f41c32dfd79427faa2d2e8955eaa
Gaiminho <leitelucas470@gmail.com>
2025-09-21 10:35:29 -0300

    style: imported the ionic module to the post-card component to be able to use the ion-icon tag

--- mobile/src/app/components/post-card/post-card.component.ts
+++ mobile/src/app/components/post-card/post-card.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'post-card',
   templateUrl: './post-card.component.html',
   styleUrls: ['./post-card.component.scss'],
   standalone: true,
+  imports: [IonicModule],
 })
 export class PostCardComponent  implements OnInit {
 
