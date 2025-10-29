90286183881a0a3ce65ecc408b9063d2b4d20f22
duxp967 <duduardo967@gmail.com>
2025-10-29 01:09:05 -0300

    register route created

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,39 @@
+//get the service of firestore
+const { db } = require("../config/db");
+
+//create an user
 exports.registerUser = async (req, res) => {
-    console.log("olá");
+    //get the info send by ionic
+    const { uid, name, dt_birthday, email } = req.body;
+
+    //validation
+    if(!uid || !name || !email) return res.status(400).json({ error: "Essential data (uid, name or email) not send!" });
+
+    try {
+        //create the document on firestore
+        const userRef = db.collection('users').doc(uid);
+        
+        await userRef.set({
+            name,
+            email,
+            dt_birthday: dt_birthday,
+            imgAccount: null,
+            bio: 'new user coaprend',
+        });
+
+        return res.status(201).json({
+            message: "User and account created!",
+            uid: uid,
+        });
+    }catch(error) { //in case of error..
+        console.error("Error on register new user at Firestore: ", error);
+        return res.status(500).json({
+            error: "Internal error",
+            uid: uid,
+        });
+    }
 }
 exports.loginUser = async (req, res)=> {
-    console.log("olá")
+    console.log("Realizando login..")
+    res.status(405).json({ message: "Essa rota deve ser acessada via firebase" })
 }
