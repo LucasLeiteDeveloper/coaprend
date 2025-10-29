0389398a3722bd62c29968feed34b20f48baa059
duxp967 <duduardo967@gmail.com>
2025-10-29 03:26:28 -0300

    firebase config saved in ionic environment.ts

--- mobile/src/environments/environment.ts
+++ mobile/src/environments/environment.ts
@@ -2,11 +2,23 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+// firebase config
+export const firebaseConfig = {
+  apiKey: "AIzaSyA434_HMqg_eKrt5I6RUbj-z4lATronsOg",
+  authDomain: "coaprend-bb2ca.firebaseapp.com",
+  projectId: "coaprend-bb2ca",
+  storageBucket: "coaprend-bb2ca.firebasestorage.app",
+  messagingSenderId: "691529444081",
+  appId: "1:691529444081:web:8b93ede890a70c326a5750",
+  measurementId: "G-GXG8VCF2RW"
+}
+
+
 export const environment = {
   production: false,
+  firebase: firebaseConfig,
   apiUrl: 'http://localhost:8000/api' // ajuste conforme backend
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
