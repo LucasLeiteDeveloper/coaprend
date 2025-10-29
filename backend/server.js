94521962582241b646bbd95efdeba921a0271122
duxp967 <duduardo967@gmail.com>
2025-10-28 22:44:32 -0300

    authController removed and authRoutes inserted in server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const app = express();
 //get the authenticateToken middleware
 const authenticateToken = require('./middlewares/authenticateToken');
 //get the authenticate controller
-const authController = require('./controllers/authController');
+const authRoutes = require('./routes/authRoutes');
 
 //configure the dotenv's variables, making they usable
 require('dotenv').config();
@@ -14,7 +14,7 @@ const { DOOR } = require('./config/config');
 //configure the express to use JSON
 app.use(express.json());
 //configure the app routes of authentication to /api
-app.use('/api', authenticateToken, authController);
+app.use('/api', authenticateToken, authRoutes);
 
 //starts the application
 app.listen(DOOR, () => {
