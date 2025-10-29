c304d8f2bbb3da90fb615190992243d436fe7682
duxp967 <duduardo967@gmail.com>
2025-10-29 17:45:03 -0300

    firebaseConfig added to environment.prod

--- mobile/src/environments/environment.prod.ts
+++ mobile/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA434_HMqg_eKrt5I6RUbj-z4lATronsOg",
+    authDomain: "coaprend-bb2ca.firebaseapp.com",
+    projectId: "coaprend-bb2ca",
+    storageBucket: "coaprend-bb2ca.firebasestorage.app",
+    messagingSenderId: "691529444081",
+    appId: "1:691529444081:web:8b93ede890a70c326a5750",
+    measurementId: "G-GXG8VCF2RW"
+  },
+  apiUrl: 'http://localhost:8000/api'
 };
