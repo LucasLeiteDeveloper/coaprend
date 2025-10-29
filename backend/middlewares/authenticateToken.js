f4251e37244e6c2e58b0054955318fee5370817c
duxp967 <duduardo967@gmail.com>
2025-10-28 22:31:28 -0300

    database config created

--- backend/middlewares/authenticateToken.js
+++ backend/middlewares/authenticateToken.js
@@ -2,5 +2,21 @@
 const { auth } = require("../config/db");
 
 async function checkAuth(req, res, next){
+    //check if was send all 
     if(!req.headers.authorization || !req.headers.authorization.startsWith("Bearer ")) return res.status(401).send("Não autorizado!");
+
+    //get the idToken
+    const idToken = req.headers.authorization.split("Bearer")[1];
+
+    try {
+        //decode the idToken
+        const decodedToken = await auth.verifyIdToken(idToken);
+
+        //insert the user's data to req
+        req.user = decodedToken;
+
+        next();
+    }catch(error){ //if there's an error
+        return res.status(403).send("Token inválido!");
+    }
 }
