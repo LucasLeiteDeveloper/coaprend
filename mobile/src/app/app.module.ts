9308bbb44d664ff2a00405c31efe5436093a6ee0
duxp967 <duduardo967@gmail.com>
2025-10-29 03:38:38 -0300

    firebase configured in app.module.ts

--- mobile/src/app/app.module.ts
+++ mobile/src/app/app.module.ts
@@ -8,6 +8,16 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 
+//imports for firebase
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { environment } from 'src/environments/environment';
+
+//initialize the app of firebase
+const app = initializeApp(environment.firebase);
+//exports the auth service to be used in another components
+export const auth = getAuth(app);
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
