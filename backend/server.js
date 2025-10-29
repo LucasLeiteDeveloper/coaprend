dcc1c49bb99c5b2bb79e5e4688a50249df67a8e8
duxp967 <duduardo967@gmail.com>
2025-10-29 00:15:12 -0300

    authenticateToken removed from server.js

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,6 @@
 const express = require('express');
 //initializes the app
 const app = express();
-//get the authenticateToken middleware
-const authenticateToken = require('./middlewares/authenticateToken');
 //get the authenticate controller
 const authRoutes = require('./routes/authRoutes');
 
@@ -14,7 +12,7 @@ const { DOOR } = require('./config/config');
 //configure the express to use JSON
 app.use(express.json());
 //configure the app routes of authentication to /api
-app.use('/api', authenticateToken, authRoutes);
+app.use('/api', authRoutes);
 
 //starts the application
 app.listen(DOOR, () => {
