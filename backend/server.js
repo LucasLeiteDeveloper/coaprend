c30310fae66457ffe54b4e4605d58db85ea10711
duxp967 <duduardo967@gmail.com>
2025-10-28 22:43:11 -0300

    server.js configured

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,21 @@
 const express = require('express');
 //initializes the app
 const app = express();
+//get the authenticateToken middleware
+const authenticateToken = require('./middlewares/authenticateToken');
+//get the authenticate controller
+const authController = require('./controllers/authController');
 
 //configure the dotenv's variables, making they usable
 require('dotenv').config();
 const { DOOR } = require('./config/config');
 
+//configure the express to use JSON
+app.use(express.json());
+//configure the app routes of authentication to /api
+app.use('/api', authenticateToken, authController);
+
+//starts the application
 app.listen(DOOR, () => {
     console.log(`server running at http://localhost:${DOOR}`)
 })
