8374803e345b1fbe1beedad424c388996ef4b75b
duxp967 <duduardo967@gmail.com>
2025-10-30 00:11:03 -0300

    cors configured in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 //import Express's module
 const express = require('express');
+//import the CORS package
+const cors = require('cors');
 //initializes the app
 const app = express();
 //get the authenticate controller
@@ -8,6 +10,13 @@ const authRoutes = require('./routes/authRoutes');
 //configure the dotenv's variables, making they usable
 require('dotenv').config();
 const { DOOR } = require('./config/config');
+
+//cors configuration
+app.use(cors({
+    origin: '*', //apply any origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', //apply any method
+    allowedHeaders: 'Content-Type,Authorization' //allow headers
+}))
 
 //configure the express to use JSON
 app.use(express.json());
