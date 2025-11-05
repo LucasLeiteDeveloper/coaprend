f0f8c2bbd003c3e2070228ca4b0b3ff9f9e08042
duxp967 <duduardo967@gmail.com>
2025-11-04 22:52:54 -0300

    route of sync created

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,11 +2,14 @@
 const express = require('express');
 const router = express.Router();
 
+//get the authenticateToken 
+const authenticateToken = require("../middlewares/authenticateToken");
 //get the authenticate controller
 const authController = require('../controllers/authController');
 
 //routes of authentication
 router.post('/register', authController.registerUser);
-router.post('/login', authController.loginUser);
+//route to be called after the login
+router.post('/auth/sync', authenticateToken, authController.syncProfile);
 
 module.exports = router;
