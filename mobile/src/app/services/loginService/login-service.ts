20fd517bf9be9714c1a2f73a85d408df4ce908ef
duxp967 <duduardo967@gmail.com>
2025-11-04 23:08:48 -0300

    sync code added to login-service on Ionic

--- mobile/src/app/services/loginService/login-service.ts
+++ mobile/src/app/services/loginService/login-service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
 import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 const auth = getAuth();
 
@@ -17,9 +18,9 @@ export class LoginService {
     return true
   }
   // URL base da sua API Laravel. Lembre-se de mudar para o endereço correto!
-  private apiUrl = 'http://localhost:8000/api'; 
+  private apiUrl = environment.apiUrl; //gets the environment api url
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private http: HttpClient) { }
 
   // Função chamada pelo componente de login
   async login(formData: any): Promise<void> {
@@ -41,5 +42,38 @@ export class LoginService {
       console.error("Error on login: ", error);
       throw error;
     }
+  }
+
+  async loginWithGoogle(): Promise<void>{
+    try {
+      //create the google provider
+      const provider = new GoogleAuthProvider();
+
+      //calls the login pop-up
+      const userCredential = await signInWithPopup(auth, provider);
+      const user = userCredential.user;
+
+      // gets the token
+      const idToken = await user.getIdToken();
+
+      //saves the token on localStorage
+      localStorage.setItem("firebaseToken", idToken);
+      console.log("Login com Google feito com sucesso!");
+
+      //calls the sync route of backend
+      await this.syncProfile(idToken);
+    } catch(error){
+      console.error("Erro ao logar com google: ", error);
+      throw Error;
+    }
+  }
+
+  //calls the backend to create the profile if it's the first login
+  private async syncProfile(idToken: string): Promise<any> {
+    //creates the header to API 
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${idToken}`);
+
+    //calls the protected route on backend
+    return this.http.post(`${this.apiUrl}/auth/sync`, {}, { headers }).toPromise()
   }
 }
