ae6693c2f48bac279f4bc64b321ee0aae991e025
Gaiminho <leitelucas470@gmail.com>
2025-11-12 12:21:09 -0300

    start making the profile page

--- mobile/src/app/pages/profile/profile.page.ts
+++ mobile/src/app/pages/profile/profile.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ScrollDetail } from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -7,7 +9,31 @@ import { Component, OnInit } from '@angular/core';
   standalone: false,
 })
 export class ProfilePage implements OnInit {
-  constructor() {}
+  public isHidden: boolean = false;
+  public postTab: boolean = true;
+  public taskTab: boolean = false;
+  public calendarTab: boolean = false;
+  private lastScrollTop: number = 0;
+  public classId: any = 0;
 
-  ngOnInit() {}
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe((params) => {
+      this.classId = params.get('id');
+    });
+  }
+  onContentScroll(event: CustomEvent<ScrollDetail>) {
+    const scrollTop = event.detail.scrollTop;
+    if (scrollTop > this.lastScrollTop && scrollTop > 50) {
+      if (!this.isHidden) {
+        this.isHidden = true;
+      }
+    } else if (scrollTop < this.lastScrollTop || scrollTop === 0) {
+      if (this.isHidden) {
+        this.isHidden = false;
+      }
+    }
+    this.lastScrollTop = scrollTop;
+  }
 }
