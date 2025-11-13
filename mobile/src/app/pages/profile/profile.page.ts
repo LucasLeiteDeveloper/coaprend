7736c64cc427f7cd4ba254ad431a4afdb8447cf9
duxp967 <duduardo967@gmail.com>
2025-11-13 02:06:43 -0300

    profileData being showed in profile page

--- mobile/src/app/pages/profile/profile.page.ts
+++ mobile/src/app/pages/profile/profile.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ScrollDetail } from '@ionic/angular';
 import { ActivatedRoute } from '@angular/router';
+import { AuthService, UserProfile } from 'src/app/services/authService/auth-service';
 
 @Component({
   selector: 'app-profile',
@@ -21,13 +22,36 @@ export class ProfilePage implements OnInit {
     items: [{ content: 'Conteudo de teste' }],
   };
 
-  constructor(private route: ActivatedRoute) {}
+  // user's data
+  public profileData!: UserProfile;
+  public isLoading: boolean = false;
 
-  ngOnInit() {
+  constructor(
+    private route: ActivatedRoute,
+    private authService: AuthService) {}
+
+  async ngOnInit() {
+    await this.loadProfileData();
+
     this.route.paramMap.subscribe((params) => {
       this.classId = params.get('id');
     });
   }
+
+  async loadProfileData(){
+    this.isLoading = true;
+
+    try {
+      this.profileData = await this.authService.getProfileData();
+    } catch(error){
+      console.error("Erro ao carregar perfil: ", error);
+
+      this.authService.showToast('Erro ao carregar o perfil!');
+    } finally {
+      this.isLoading = false;
+    }
+  }
+
   onContentScroll(event: CustomEvent<ScrollDetail>) {
     const scrollTop = event.detail.scrollTop;
     if (scrollTop > this.lastScrollTop && scrollTop > 50) {
