a460b4d40a03602901f3ba37c739050fc964bdec
duxp967 <duduardo967@gmail.com>
2025-11-13 04:04:28 -0300

    edit-profile code created

--- mobile/src/app/pages/edit-profile/edit-profile.page.ts
+++ mobile/src/app/pages/edit-profile/edit-profile.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { AuthService, UserProfile } from 'src/app/services/authService/auth-service';
 
 @Component({
   selector: 'app-edit-profile',
@@ -6,10 +8,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-profile.page.scss'],
 })
 export class EditProfilePage implements OnInit {
+  // receive the user profile data
+  @Input() profile: UserProfile = {} as UserProfile;
 
-  constructor() { }
+  public dtBirthdayModel: string = '';
+  public isLoading: boolean = false;
+
+  constructor(
+    private modalCtrl: ModalController,
+    private authService: AuthService
+  ) { }
 
   ngOnInit() {
+    if(this.profile.dt_birthday){ //converts the dt_birthday
+      const dateObj = this.profile.dt_birthday instanceof Date
+        ? this.profile.dt_birthday
+        : new Date(this.profile.dt_birthday)
+
+      this.dtBirthdayModel = dateObj.toISOString().split('T')[0];
+    }
   }
+
+  //save all changes
+  async saveChanges(){
+    this.isLoading = true;
+
+    try {
+      const updates = {
+        name: this.profile.name,
+        bio: this.profile.bio,
+        username: this.profile.username,
+        dt_birthday: this.dtBirthdayModel
+      }
+
+      await this.authService.updateProfileData(updates);
+
+      // stop the modal and return true to indicate that the update was successfuly
+      this.modalCtrl.dismiss(true);
+    } catch (error) {
+      this.authService.showToast("Erro ao salvar, tente novamente mais tarde");
+      this.modalCtrl.dismiss(false); // close with failure
+    } finally{ this.isLoading = false; }
+  }
+
+  close(){ this.modalCtrl.dismiss() }
 
 }
