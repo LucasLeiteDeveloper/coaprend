563c917293bdffc910abdc45f4d7ffba316ab65b
duxp967 <duduardo967@gmail.com>
2025-11-13 14:48:51 -0300

    class being created

--- mobile/src/app/pages/create/class/class.page.ts
+++ mobile/src/app/pages/create/class/class.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/authService/auth-service';
+import { ContentService } from 'src/app/services/contentService/content-service';
 
 @Component({
   selector: 'app-class',
@@ -7,7 +10,42 @@ import { Component, OnInit } from '@angular/core';
   standalone: false,
 })
 export class ClassPage implements OnInit {
-  constructor() {}
+  isLoading: boolean = false;
+
+  //form variables
+  title: string = '';
+  description: string = '';
+
+  constructor(
+    private contentService: ContentService,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit() {}
+
+  async submitCreateRoom(){
+    if( this.title.trim().length < 3 ){
+      this.authService.showToast("O título da sala é obrigatório!");
+      return;
+    }
+
+    this.isLoading = true;
+    try {
+      const response = await this.contentService.createRoom({
+        title: this.title,
+        description: this.description
+      });
+
+      const newRoomId = response.roomId;
+
+      this.authService.showToast("Classe criada com sucesso!");
+      this.router.navigate(['/class', newRoomId, 'posts'])
+    } catch(error){
+      console.error("Erro ao criar a sala", error);
+      this.authService.showToast("Erro ao criar a sala! Tente novamente mais tarde")
+    } finally{
+      this.isLoading = false;
+    }
+  }
 }
