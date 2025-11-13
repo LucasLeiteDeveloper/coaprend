94057c7a4bb747de0eae795f329c49ba5a9c6a0f
duxp967 <duduardo967@gmail.com>
2025-11-13 12:41:58 -0300

    content Routes updated

--- backend/routes/contentRoutes.js
+++ backend/routes/contentRoutes.js
@@ -6,6 +6,15 @@ const authenticateToken = require("../middlewares/authenticateToken");
 //get the controller
 const contentController = require("../controllers/contentController");
 
+// route of rooms
 router.post("/room", authenticateToken, contentController.createContentRoom);
+
+// posts routes
+router.post("/posts", authenticateToken, contentController.createPost);
+router.get("/rooms/:roomId/posts", authenticateToken, contentController.getPostsForRoom);
+
+// tasks routes
+router.post("/tasks", authenticateToken, contentController.createTask);
+router.get("/rooms/:roomId/tasks", authenticateToken, contentController.getTasksForRoom);
 
 module.exports = router;
