780b2dccae70bf7bc8379a65d246c101ba27683e
duxp967 <duduardo967@gmail.com>
2025-11-13 12:55:58 -0300

    last_day attribute transformed in dt_final

--- mobile/src/app/services/contentService/content-service.ts
+++ mobile/src/app/services/contentService/content-service.ts
@@ -1,8 +1,49 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { AuthService } from '../authService/auth-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContentService {
+  //the API url prepared to the content
+  private readonly apiUrl = `${environment.apiUrl}/content`;
   
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService
+  ){ }
+
+  // gets the header of authentication
+  private getAuthHeaders(): HttpHeaders{
+    const idToken = localStorage.getItem("firebaseToken");
+
+    if(!idToken){ // if the idToken isn't valid
+      this.authService.showToast("Sessão expirada, faça login novamente!");
+      throw new Error("Usuário não autenticado!");
+    }
+
+    return new HttpHeaders().set("Authorization", `Bearer ${idToken}`);
+  }
+
+  // methods for posts
+  async getPosts(roomId: string): Promise<any[] | undefined> {
+    const headers = this.getAuthHeaders();
+
+    return this.http.get<any[]>(`${this.apiUrl}/rooms/${roomId}/posts`, { headers }).toPromise();
+  }
+  async createPost(data: { title: string, roomId: string, texts: string[] }): Promise<any> {
+    const headers = this.getAuthHeaders();
+
+    return this.http.post(`${this.apiUrl}/posts`, data, { headers }).toPromise();
+  }
+
+  //tasks methods
+  async getTasks(roomId: string): Promise<any[] | undefined> {
+    const headers = this.getAuthHeaders();
+
+    return this.http.get<any[]>(`${this.apiUrl}/rooms/${roomId}/tasks`, { headers }).toPromise();
+  }
+  async createTask(data: { title: string, roomId: string, last_date: string }): Promise<>
 }
