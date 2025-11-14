08c7ad3410299781c5d1a03ccdace645d830f177
Gaiminho <leitelucas470@gmail.com>
2025-11-14 16:59:15 -0300

    finished the forgot-password page

--- mobile/src/app/app-routing.module.ts
+++ mobile/src/app/app-routing.module.ts
@@ -8,11 +8,6 @@ const routes: Routes = [
     pathMatch: 'full',
   },
   {
-    path: 'agenda',
-    loadChildren: () =>
-      import('./oldPages/agenda/agenda.module').then((m) => m.AgendaPageModule),
-  },
-  {
     path: 'policy',
     loadChildren: () =>
       import('./pages/static/policy/policy.module').then(
@@ -40,55 +35,77 @@ const routes: Routes = [
   },
   {
     path: 'class/:id',
-    loadChildren: () => import('./pages/class/class.module').then( m => m.ClassPageModule)
+    loadChildren: () =>
+      import('./pages/class/class.module').then((m) => m.ClassPageModule),
   },
   {
     path: 'create-post',
-    loadChildren: () => import('./oldPages/create-post/create-post.module').then( m => m.PostCreatePageModule)
+    loadChildren: () =>
+      import('./oldPages/create-post/create-post.module').then(
+        (m) => m.PostCreatePageModule
+      ),
   },
   {
     path: 'create-task',
-    loadChildren: () => import('./oldPages/create-task/create-task.module').then( m => m.CreateTaskPageModule)
+    loadChildren: () =>
+      import('./oldPages/create-task/create-task.module').then(
+        (m) => m.CreateTaskPageModule
+      ),
   },
   {
     path: 'post/:id',
-    loadChildren: () => import('./pages/create/post/post.module').then( m => m.PostPageModule)
+    loadChildren: () =>
+      import('./pages/create/post/post.module').then((m) => m.PostPageModule),
   },
   {
     path: 'task/:id',
-    loadChildren: () => import('./pages/create/task/task.module').then( m => m.TaskPageModule)
+    loadChildren: () =>
+      import('./pages/create/task/task.module').then((m) => m.TaskPageModule),
   },
   {
     path: 'class',
-    loadChildren: () => import('./pages/create/class/class.module').then( m => m.ClassPageModule)
+    loadChildren: () =>
+      import('./pages/create/class/class.module').then(
+        (m) => m.ClassPageModule
+      ),
   },
   {
     path: 'tag',
-    loadChildren: () => import('./pages/create/tag/tag.module').then( m => m.TagPageModule)
+    loadChildren: () =>
+      import('./pages/create/tag/tag.module').then((m) => m.TagPageModule),
   },
   {
     path: 'tfa',
-    loadChildren: () => import('./pages/auth/tfa/tfa.module').then( m => m.TfaPageModule)
+    loadChildren: () =>
+      import('./pages/auth/tfa/tfa.module').then((m) => m.TfaPageModule),
+  },
+  {
+    path: 'forgot-password',
+    loadChildren: () =>
+      import('./pages/auth/forgot-password/forgot-password.module').then((m) => m.ForgotPasswordPageModule),
   },
   {
     path: 'profile',
-    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)
+    loadChildren: () =>
+      import('./pages/profile/profile.module').then((m) => m.ProfilePageModule),
   },
   {
     path: 'post',
-    loadChildren: () => import('./pages/post/post.module').then( m => m.PostPageModule)
+    loadChildren: () =>
+      import('./pages/post/post.module').then((m) => m.PostPageModule),
   },
   {
     path: 'task',
-    loadChildren: () => import('./pages/task/task.module').then( m => m.TaskPageModule)
-  },  {
-    path: 'create-class',
-    loadChildren: () => import('./pages/create-class/create-class.module').then( m => m.CreateClassPageModule)
+    loadChildren: () =>
+      import('./pages/task/task.module').then((m) => m.TaskPageModule),
   },
-
-
-
-
+  {
+    path: 'create-class',
+    loadChildren: () =>
+      import('./pages/create-class/create-class.module').then(
+        (m) => m.CreateClassPageModule
+      ),
+  },
 ];
 
 @NgModule({
