7c8991d1420bd1b9843ce8de738012816b8493f6
Darth-Gabs <almeidagabriel1962@gmail.com>
2025-11-14 17:23:13 -0300

    create posts

--- mobile/src/app/app-routing.module.ts
+++ mobile/src/app/app-routing.module.ts
@@ -8,6 +8,13 @@ const routes: Routes = [
     pathMatch: 'full',
   },
   {
+    path: 'agenda',
+    loadChildren: () =>
+      import('./oldPages/agenda/agenda.module').then(
+        (m) => m.AgendaPageModule
+      ),
+  },
+  {
     path: 'policy',
     loadChildren: () =>
       import('./pages/static/policy/policy.module').then(
@@ -24,7 +31,9 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () =>
-      import('./pages/auth/login/login.module').then((m) => m.LoginPageModule),
+      import('./pages/auth/login/login.module').then(
+        (m) => m.LoginPageModule
+      ),
   },
   {
     path: 'register',
@@ -36,13 +45,15 @@ const routes: Routes = [
   {
     path: 'class/:id',
     loadChildren: () =>
-      import('./pages/class/class.module').then((m) => m.ClassPageModule),
+      import('./pages/class/class.module').then(
+        (m) => m.ClassPageModule
+      ),
   },
   {
     path: 'create-post',
     loadChildren: () =>
       import('./oldPages/create-post/create-post.module').then(
-        (m) => m.PostCreatePageModule
+        (m) => m.CreatePostPageModule
       ),
   },
   {
@@ -55,12 +66,16 @@ const routes: Routes = [
   {
     path: 'post/:id',
     loadChildren: () =>
-      import('./pages/create/post/post.module').then((m) => m.PostPageModule),
+      import('./pages/create/post/post.module').then(
+        (m) => m.PostPageModule
+      ),
   },
   {
     path: 'task/:id',
     loadChildren: () =>
-      import('./pages/create/task/task.module').then((m) => m.TaskPageModule),
+      import('./pages/create/task/task.module').then(
+        (m) => m.TaskPageModule
+      ),
   },
   {
     path: 'class',
@@ -72,32 +87,37 @@ const routes: Routes = [
   {
     path: 'tag',
     loadChildren: () =>
-      import('./pages/create/tag/tag.module').then((m) => m.TagPageModule),
+      import('./pages/create/tag/tag.module').then(
+        (m) => m.TagPageModule
+      ),
   },
   {
     path: 'tfa',
     loadChildren: () =>
-      import('./pages/auth/tfa/tfa.module').then((m) => m.TfaPageModule),
-  },
-  {
-    path: 'forgot-password',
-    loadChildren: () =>
-      import('./pages/auth/forgot-password/forgot-password.module').then((m) => m.ForgotPasswordPageModule),
+      import('./pages/auth/tfa/tfa.module').then(
+        (m) => m.TfaPageModule
+      ),
   },
   {
     path: 'profile',
     loadChildren: () =>
-      import('./pages/profile/profile.module').then((m) => m.ProfilePageModule),
+      import('./pages/profile/profile.module').then(
+        (m) => m.ProfilePageModule
+      ),
   },
   {
     path: 'post',
     loadChildren: () =>
-      import('./pages/post/post.module').then((m) => m.PostPageModule),
+      import('./pages/post/post.module').then(
+        (m) => m.PostPageModule
+      ),
   },
   {
     path: 'task',
     loadChildren: () =>
-      import('./pages/task/task.module').then((m) => m.TaskPageModule),
+      import('./pages/task/task.module').then(
+        (m) => m.TaskPageModule
+      ),
   },
   {
     path: 'create-class',
@@ -110,7 +130,9 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+    RouterModule.forRoot(routes, {
+      preloadingStrategy: PreloadAllModules,
+    }),
   ],
   exports: [RouterModule],
 })
