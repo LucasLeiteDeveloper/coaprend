43b82b3215b3b81b5116fcafb19e54dbcc80ca6a
Gaiminho <leitelucas470@gmail.com>
2025-11-17 12:13:06 -0300

    style: optimized the class page code for better readbility

---
+++ mobile/src/app/services/hideOnScrollService/hide-on-scroll-service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { HideOnScrollService } from './hide-on-scroll-service';
+
+describe('HideOnScrollService', () => {
+  let service: HideOnScrollService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(HideOnScrollService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
