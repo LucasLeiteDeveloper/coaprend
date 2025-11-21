e98c391dc88b671433b290867723dfb20b42e0b7
Gaiminho <leitelucas470@gmail.com>
2025-11-21 16:32:21 -0300

    adde dynamic pages for the class config and added a confirmation alert for exiting a class

--- mobile/src/app/pages/class-select/class-select.page.ts
+++ mobile/src/app/pages/class-select/class-select.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-class-select',
@@ -7,10 +8,54 @@ import { Component, OnInit } from '@angular/core';
   standalone: false,
 })
 export class ClassSelectPage implements OnInit {
+  public classes: any = [
+    {
+      name: "teste",
+      id: 1,
+    },
+    {
+      name: "teste2",
+      id: 2,
+    },
+    {
+      name: "teste2",
+      id: 2,
+    },
+    {
+      name: "teste2",
+      id: 2,
+    }
+  ]
 
-  constructor() { }
+  constructor(private alert: AlertController,) { }
 
   ngOnInit() {
+  }
+
+  async exitClassAlert(name: string, id: any) {
+    const alert = await this.alert.create({
+      header: `Deseja sair de ${name}?`,
+      // subHeader: 'A Sub Header Is Optional',
+      // message: 'A message should be a short, complete sentence.',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Alert canceled');
+          },
+        },
+        {
+          text: 'OK',
+          role: 'confirm',
+          handler: () => {
+            console.log('Alert confirmed');
+          },
+        },
+      ]
+    });
+
+    await alert.present();
   }
 
 }
