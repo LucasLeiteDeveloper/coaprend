70127fa4a5e78da5fb903a240964ebde9b014596
Gaiminho <leitelucas470@gmail.com>
2025-11-21 12:10:19 -0300

    improved the profile page

--- mobile/src/app/pages/profile/profile-routing.module.ts
+++ mobile/src/app/pages/profile/profile-routing.module.ts
@@ -7,7 +7,11 @@ const routes: Routes = [
   {
     path: '',
     component: ProfilePage
+  },  {
+    path: 'saved',
+    loadChildren: () => import('./saved/saved.module').then( m => m.SavedPageModule)
   }
+
 ];
 
 @NgModule({
