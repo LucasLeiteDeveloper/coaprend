15bfb0f6e2b2d5cb1479d671b314dd29ad9e69ce
Darth-Gabs <almeidagabriel1962@gmail.com>
2025-11-22 19:27:57 -0300

    start of config, not complete

--- mobile/src/app/pages/class/config/config.page.ts
+++ mobile/src/app/pages/class/config/config.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ClassService } from 'src/app/services/classService/class';
+import { AlertController, ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-config',
@@ -8,9 +11,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfigPage implements OnInit {
 
-  constructor() { }
+  classId!: number;
+  classData: any;
+
+  constructor(
+    private route: ActivatedRoute,
+    private classService: ClassService,
+    private alertCtrl: AlertController,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    this.classId = Number(this.route.snapshot.paramMap.get('id'));
+    this.loadClass();
   }
 
+  loadClass() {
+    this.classService.getClassById(this.classId).subscribe(res => {
+      this.classData = res;
+    });
+  }
+
+  // ---------- BÁSICO ----------
+  editName() {
+    this.router.navigate(['/edit-class-name', this.classId]);
+  }
+
+  editPhoto() {
+    this.router.navigate(['/edit-class-photo', this.classId]);
+  }
+
+  editDescription() {
+    this.router.navigate(['/edit-class-description', this.classId]);
+  }
+
+  // ---------- ORGANIZAÇÃO ----------
+  openTags() {
+    this.router.navigate(['/class-tags', this.classId]);
+  }
+
+  // ---------- PERMISSÕES ----------
+  openUsers() {
+    this.router.navigate(['/class-users', this.classId]);
+  }
+
+  // ---------- CÓDIGO DE CONVITE ----------
+  async showInviteCode() {
+    const alert = await this.alertCtrl.create({
+      header: 'Código da sala',
+      message: `<b>${this.classData?.code}</b>`,
+      buttons: [
+        {
+          text: 'Regenerar',
+          handler: () => {
+            this.regenerateCode();
+          },
+        },
+        { text: 'Fechar' }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  regenerateCode() {
+    this.classService.regenerateCode(this.classId).subscribe(res => {
+      this.classData.code = res.code;
+      this.showInviteCode();
+    });
+  }
 }
