5e50008def222e1aab2458de28b21fbd97d524e0
duxp967 <duduardo967@gmail.com>
2025-11-22 16:36:50 -0300

    contentController updated with updating functions

--- backend/controllers/contentController.js
+++ backend/controllers/contentController.js
@@ -152,3 +152,118 @@ exports.getTasksForRoom = async (req, res) => {
         return res.status(500).json({ error: "Erro interno" })
     }
 }
+
+// UPDATING ROOMS
+exports.updateRoom = async (req, res)=> {
+    try {
+        const { roomId } = req.params; //gets the id of room
+        const updates = req.body; // can have title, description, icon and/or config
+        const userId = req.user.id; // get the user id
+
+        // gets a reference of the room
+        const roomRef = db.collection('room').doc(roomId);
+        const doc = await roomRef.get();
+
+        // check existence
+        if (!doc.exists) return res.status(404).json({ error: "Sala não encontrada!" });
+
+        // only the creator can edit the config
+        if (doc.data().creatorUid !== userId) return res.status(403).json({ error: "Apenas o dono da sala pode editá-la" });
+
+        // remove dangerous fields of body if exists
+        delete updates.creatorUid;
+        delete updates.id;
+
+        await roomRef.update(updates); //update only the send fields
+
+        return res.status(200).json({ message: "Sala atualizada com sucesso!" });
+    } catch(error) {
+        console.error("Erro updateRoom: ", error);
+        return res.status(500).json({ error: "Erro interno!" });
+    }
+}
+
+// UPDATING POSTS
+exports.updatePost = async (req, res) => {
+    try {
+        const { postId } = req.params; // gets the post id
+        const updates = req.body; //get the updates 
+        const userId = req.user.uid; //gets the user id
+
+        //crete a reference of the collection
+        const postRef = db.collection('posts').doc(postId); 
+        const doc = await postRef.get();
+
+        // checking the existence
+        if (!doc.exists) return res.status(404).json({ error: "Post não encontrado." });
+
+        // only the author of the post can update it
+        if (doc.data().authorUid !== userId) return res.status(403).json({ error: "Você só pode editar seus próprios posts." });
+
+        // upodates the date of modification
+        updates.dt_updated = new Date();
+        // cannot change the author
+        delete updates.authorUid;
+
+        // update the post
+        await postRef.update(updates);
+
+        return res.status(200).json({ message: "Post atualizado!" });
+    } catch(error){
+        console.error("Erro update post:", error);
+        return res.status(500).json({ error: "Erro interno." });
+    }
+}
+
+// UPDATING TASKS
+exports.updateTask = async (req, res) => {
+    try {
+        const { taskId } = req.params; //gets the task id
+        const updates = req.body; //gets the update
+        
+        // if has some date, change to Date()
+        if (updates.dt_final) updates.dt_final = new Date(updates.dt_final);
+
+        // create a reference of the task and update it
+        const taskRef = db.collection('tasks').doc(taskId);
+        await taskRef.update(updates);
+
+        return res.status(200).json({ message: "Tarefa atualizada!" });
+    } catch (error) {
+        console.error("Erro update task:", error);
+        return res.status(500).json({ error: "Erro interno." });
+    }
+}
+
+// search POSTS (by word)
+exports.searchGlobalPosts = async (req, res) => {
+    try {
+        const { q } = req.query; // gets ?q=word
+        
+        // checking existence
+        if (!q) return res.status(400).json({ error: "Termo de busca obrigatório." });
+
+        //create a reference to all posts
+        const postsRef = db.collection('posts');
+        
+        //get all the posts
+        const snapshot = await postsRef.get();
+        const allPosts = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+
+        // filter the posts
+        const term = q.toLowerCase();
+        const filteredPosts = allPosts.filter(post => {
+            const titleMatch = post.title && post.title.toLowerCase().includes(term);
+            // check if texts is an Array an if some text has the term
+            const contentMatch = post.texts && Array.isArray(post.texts) && post.texts.some(t => t.toLowerCase().includes(term));
+            
+            return titleMatch || contentMatch;
+        });
+
+        return res.status(200).json(filteredPosts);
+
+    } catch (error) {
+        console.error("Erro na busca:", error);
+        return res.status(500).json({ error: "Erro interno na busca." });
+    }
+}
