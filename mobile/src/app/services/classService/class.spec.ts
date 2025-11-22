b400e63f30efb19bacabb282d666fea62fd8ef28
duxp967 <duduardo967@gmail.com>
2025-11-22 18:15:29 -0300

    class.spec.ts merge conflicts corrected

--- mobile/src/app/services/classService/class.spec.ts
+++ mobile/src/app/services/classService/class.spec.ts
@@ -1,15 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
-<<<<<<<< HEAD:mobile/src/app/services/taskService/task.spec.ts
-import { TaskService } from './task';
-
-describe('TaskService', () => {
-  let service: TaskService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(TaskService);
-========
 import { ClassService } from './class';
 
 describe('Class', () => {
@@ -18,7 +7,6 @@ describe('Class', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(ClassService);
->>>>>>>> 5ecd4927ffe6236613e585ed0fb126130f7a86f3:mobile/src/app/services/classService/class.spec.ts
   });
 
   it('should be created', () => {
