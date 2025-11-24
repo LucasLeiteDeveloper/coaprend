bff4ee9e289a442f646f75fc7c7325299dde4d96
Gaiminho <leitelucas470@gmail.com>
2025-11-24 18:34:04 -0300

    select tags in the class page now works

--- mobile/src/app/pages/class/class.page.ts
+++ mobile/src/app/pages/class/class.page.ts
@@ -3,6 +3,7 @@ import { PopoverController } from '@ionic/angular';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MenuCriacaoComponent } from 'src/app/components/menu-criacao/menu-criacao.component';
 import { HideOnScrollService } from 'src/app/services/hideOnScrollService/hide-on-scroll-service';
+import { TagService } from 'src/app/services/tagService/tag';
 
 @Component({
   selector: 'app-class',
@@ -13,13 +14,15 @@ import { HideOnScrollService } from 'src/app/services/hideOnScrollService/hide-o
 
 export class ClassPage implements OnInit {
   public selectedTab: string = "";
-  public classId: any = "";
+  public classId: any;
+  public tags: any;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private popoverCtrl: PopoverController,
     public scroll: HideOnScrollService,
+    public tag: TagService,
   ) {}
 
   ngOnInit() {
@@ -27,6 +30,30 @@ export class ClassPage implements OnInit {
       this.classId = params.get('id');
     });
     this.selectedTab = this.router.url.split('/')[3];
+    // this.tags = this.tag.getTagsByClass(this.classId);
+    this.tags = [
+      {
+        id: 1,
+        icon: "pricetag-outline",
+        text: "Oi",
+        color: "blue",
+        selected: false,
+      },
+      {
+        id: 2,
+        icon: "pricetag-outline",
+        text: "Oi2",
+        color: "red",
+        selected: false,
+      },
+      {
+        id: 3,
+        icon: "pricetag-outline",
+        text: "Oi3",
+        color: "yellow",
+        selected: false,
+      }
+    ];
   }
   
   onSelectClass(id: any) {
@@ -47,5 +74,16 @@ export class ClassPage implements OnInit {
       cssClass: 'menu-criacao-popover',
     });
     await popover.present();
+  }
+
+  selectTag(id: any) {
+    const tagElement = document.getElementById("tag"+id);
+    if (!tagElement?.classList.contains('selected')) {
+      tagElement?.classList.add("selected");
+      this.tags.find((tag: any) => tag.id == id).selected = true;
+    } else {
+      tagElement?.classList.remove("selected");
+      this.tags.find((tag: any) => tag.id == id).selected = false;
+    }
   }
 }
