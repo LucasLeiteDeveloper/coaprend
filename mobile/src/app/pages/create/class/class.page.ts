4cf175aeb3190fe1af516e6ed059ecf169822110
duxp967 <duduardo967@gmail.com>
2025-11-27 02:47:41 -0300

    receiving classData on creation

--- mobile/src/app/pages/create/class/class.page.ts
+++ mobile/src/app/pages/create/class/class.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastController } from '@ionic/angular';
 import { ClassService } from 'src/app/services/classService/class';
@@ -9,19 +9,25 @@ import { ClassService } from 'src/app/services/classService/class';
   styleUrls: ['./class.page.scss'],
   standalone: false,
 })
-export class CreateClassPage {
-  className: string = '';
-  imageFile?: File;
-
-  // Tags
-  tags: string[] = [];
-  newTag: string = '';
+export class CreateClassPage implements OnInit{
+  classData = {
+    title: '',
+    description: '',
+    icon: '',
+    tags: [] as string[]
+  };
+  newTag!: string;
 
   constructor(
     private classService: ClassService,
     private toastController: ToastController,
     private router: Router
   ) {}
+
+  async ngOnInit() {
+      this.showData();
+  }
+  showData(){ console.table(this.classData) }
 
   // Abrir seletor de arquivo
   public openFileDialog = () => {
@@ -31,38 +37,40 @@ export class CreateClassPage {
   // Selecionar imagem
   public setImage = (_event: any) => {
     const f = _event.target.files![0];
-    if (f) this.imageFile = f;
+    if (f) this.classData.icon = f;
   };
 
   // Adicionar tag
   addTag() {
     if (!this.newTag.trim()) return;
-    this.tags.push(this.newTag.trim());
+    this.classData.tags.push(this.newTag.trim());
     this.newTag = '';
+
+    this.showData();
   }
 
   // Remover tag
   removeTag(index: number) {
-    this.tags.splice(index, 1);
+    this.classData.tags.splice(index, 1);
   }
 
   // Criar sala
   async createClass() {
-    if (!this.className.trim()) {
+    if (!this.classData.title.trim()) {
       this.showToast('O nome da sala é obrigatório.');
       return;
     }
 
     const form = new FormData();
-    form.append('nome', this.className);
+    form.append('nome', this.classData.title);
     form.append('descricao', '');
     
-    if (this.imageFile) {
-      form.append('imagem', this.imageFile);
+    if (this.classData.icon) {
+      form.append('imagem', this.classData.icon);
     }
 
-    if (this.tags.length > 0) {
-      this.tags.forEach((t, i) => form.append(`tags[${i}]`, t));
+    if (this.classData.tags.length > 0) {
+      this.classData.tags.forEach((t, i) => form.append(`tags[${i}]`, t));
     }
 
     this.classService.createClass(form).subscribe({
