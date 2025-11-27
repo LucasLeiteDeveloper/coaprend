d3d4c0001bd91e4a6325a2e944ac8bacc0f434fd
duxp967 <duduardo967@gmail.com>
2025-11-26 23:55:06 -0300

    ERRO ON SHOW PROFILE DATA ON PROFILE PAGE

--- mobile/src/app/pages/profile/profile.page.ts
+++ mobile/src/app/pages/profile/profile.page.ts
@@ -3,6 +3,7 @@ import { HideOnScrollService } from 'src/app/services/hideOnScrollService/hide-o
 import { ActivatedRoute } from '@angular/router';
 import { PopoverController } from '@ionic/angular';
 import { MenuCriacaoComponent } from 'src/app/components/menu-criacao/menu-criacao.component';
+import { AuthService, UserProfile } from 'src/app/services/authService/auth-service';
 
 @Component({
   selector: 'app-profile',
@@ -18,16 +19,37 @@ export class ProfilePage implements OnInit {
     items: [{ content: 'Conteudo de teste' }],
   };
 
+  public isLoading: boolean = false;
+  public profileData!: UserProfile;
+
   constructor(
     private route: ActivatedRoute,
+    private authService: AuthService,
     private popover: PopoverController,
     public scroll: HideOnScrollService,
   ) {}
 
-  ngOnInit() {
+  async ngOnInit() {
+    await this.loadProfileData();
+    console.log(this.profileData);
+
     this.route.paramMap.subscribe((params) => {
       this.classId = params.get('id');
     });
+  }
+
+  async loadProfileData() {
+    this.isLoading = true;
+
+    try {
+      this.profileData = await this.authService.getProfileData();
+    } catch(error){
+      console.error("Erro ao carregar perfil: ", error);
+
+      this.authService.showToast('Erro ao carregar o perfil!');
+    } finally {
+      this.isLoading = false;
+    }
   }
 
   async openCreateNav(ev: any) {
