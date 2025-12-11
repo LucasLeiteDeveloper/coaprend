1d10f6402ac56b281c8b428a384b3f3a2b01993a
duxp967 <duduardo967@gmail.com>
2025-12-11 09:19:52 -0300

    getting classData in class page

--- mobile/src/app/pages/class/config/config.page.ts
+++ mobile/src/app/pages/class/config/config.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ClassService } from 'src/app/services/classService/class';
 import { TagService } from 'src/app/services/tagService/tag';
 import { ToastController, NavController } from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
+import { ContentService } from 'src/app/services/contentService/content-service';
 
 @Component({
   selector: 'app-config',
@@ -11,7 +13,7 @@ import { ToastController, NavController } from '@ionic/angular';
 })
 export class ConfigPage implements OnInit {
 
-  classId!: number;
+  classId!: string;
   classData: any = {};
   inviteCode = '';
 
@@ -32,14 +34,18 @@ export class ConfigPage implements OnInit {
   showTagEditor = false;
 
   constructor(
+    private contentService: ContentService,
     private classService: ClassService,
     private tagService: TagService,
     private toastCtrl: ToastController,
-    private navCtrl: NavController
+    private navCtrl: NavController,
+    private route: ActivatedRoute
   ) {}
 
 
   ngOnInit() {
+    this.classId = this.route.snapshot.params['id'] || '';
+
     this.loadClassData();
   }
 
@@ -47,18 +53,33 @@ export class ConfigPage implements OnInit {
   // CARREGAR DADOS DA SALA
   // ============================
 
-  loadClassData() {
-    this.classService.getCurrentUserClass().subscribe({
-      next: (data) => {
-        this.classData = data;
-        this.classId = data.id;
-        this.inviteCode = data.invite_code;
-        this.newName = data.name;
+  async loadClassData() {
+    try {
+      const response = await this.contentService.getClassDetails(this.classId);
+      console.log("Sala: ", response)
 
-        this.loadTags();
-      },
-      error: () => this.showToast('Erro ao carregar dados da sala.')
-    });
+      if(response) {
+        this.classData = response;
+        this.inviteCode = response.code;
+        this.newName = response.title;
+        this.tags = response.tags;
+      }
+    } catch(error) {
+      this.showToast("Erro ao carregar dados da classe");
+      console.log("Erro ao carregar classe: ", error);
+    }
+
+    // this.classService.getCurrentUserClass().subscribe({
+    //   next: (data) => {
+    //     this.classData = data;
+    //     this.classId = data.id;
+    //     this.inviteCode = data.invite_code;
+    //     this.newName = data.name;
+
+    //     this.loadTags();
+    //   },
+    //   error: () => this.showToast('Erro ao carregar dados da sala.')
+    // });
   }
 
   // ============================
@@ -71,13 +92,13 @@ export class ConfigPage implements OnInit {
   }
 
   regenerateCode() {
-    this.classService.regenerateCode(this.classId).subscribe({
-      next: (res) => {
-        this.inviteCode = res.invite_code;
-        this.showToast('Novo código gerado.');
-      },
-      error: () => this.showToast('Erro ao gerar novo código.')
-    });
+    // this.classService.regenerateCode(this.classId).subscribe({
+    //   next: (res) => {
+    //     this.inviteCode = res.invite_code;
+    //     this.showToast('Novo código gerado.');
+    //   },
+    //   error: () => this.showToast('Erro ao gerar novo código.')
+    // });
   }
 
   // ============================
@@ -87,10 +108,10 @@ export class ConfigPage implements OnInit {
   updateName() {
     const payload = { name: this.newName };
 
-this.classService.updateClassName(this.classId, this.newName).subscribe({
-  next: () => this.showToast('Nome atualizado!'),
-  error: () => this.showToast('Erro ao atualizar nome.')
-});
+  // this.classService.updateClassName(this.classId, this.newName).subscribe({
+  //   next: () => this.showToast('Nome atualizado!'),
+  //   error: () => this.showToast('Erro ao atualizar nome.')
+  // });
   }
 
   // ============================
@@ -112,10 +133,10 @@ updatePhoto() {
   reader.onload = () => {
     const base64 = (reader.result as string).split(',')[1]; // remove cabeçalho data:image/...
 
-    this.classService.updateClassImage(this.classId, base64).subscribe({
-      next: () => this.showToast('Foto atualizada!'),
-      error: () => this.showToast('Erro ao atualizar foto.')
-    });
+    // this.classService.updateClassImage(this.classId, base64).subscribe({
+    //   next: () => this.showToast('Foto atualizada!'),
+    //   error: () => this.showToast('Erro ao atualizar foto.')
+    // });
   };
 
   reader.onerror = () => {
@@ -131,10 +152,7 @@ updatePhoto() {
   // ============================
 
   loadTags() {
-    this.tagService.getTagsByClass(this.classId).subscribe({
-      next: (tags) => this.tags = tags,
-      error: () => this.showToast('Erro ao carregar tags.')
-    });
+    
   }
 
   createTag() {
@@ -145,34 +163,34 @@ updatePhoto() {
       color: this.newTagColor
     };
 
-    this.tagService.createTag(this.classId, payload).subscribe({
-      next: () => {
-        this.newTagName = '';
-        this.newTagColor = '#3B82F6';
-        this.loadTags();
-        this.showToast('Tag criada!');
-      },
-      error: () => this.showToast('Erro ao criar tag.')
-    });
+    // this.tagService.createTag(this.classId, payload).subscribe({
+    //   next: () => {
+    //     this.newTagName = '';
+    //     this.newTagColor = '#3B82F6';
+    //     this.loadTags();
+    //     this.showToast('Tag criada!');
+    //   },
+    //   error: () => this.showToast('Erro ao criar tag.')
+    // });
   }
 
   updateTag(tag: any) {
     const payload = { name: tag.name, color: tag.color };
 
-    this.tagService.updateTag(this.classId, tag.id, payload).subscribe({
-      next: () => this.showToast('Tag atualizada!'),
-      error: () => this.showToast('Erro ao atualizar tag.')
-    });
+    // this.tagService.updateTag(this.classId, tag.id, payload).subscribe({
+    //   next: () => this.showToast('Tag atualizada!'),
+    //   error: () => this.showToast('Erro ao atualizar tag.')
+    // });
   }
 
   deleteTag(tagId: number) {
-    this.tagService.deleteTag(this.classId, tagId).subscribe({
-      next: () => {
-        this.tags = this.tags.filter(t => t.id !== tagId);
-        this.showToast('Tag removida!');
-      },
-      error: () => this.showToast('Erro ao remover tag.')
-    });
+    // this.tagService.deleteTag(this.classId, tagId).subscribe({
+    //   next: () => {
+    //     this.tags = this.tags.filter(t => t.id !== tagId);
+    //     this.showToast('Tag removida!');
+    //   },
+    //   error: () => this.showToast('Erro ao remover tag.')
+    // });
   }
 
   // ============================
@@ -180,13 +198,13 @@ updatePhoto() {
   // ============================
 
   leaveClass() {
-    this.classService.leaveClass(this.classId).subscribe({
-      next: () => {
-        this.showToast('Você saiu da sala.');
-        this.navCtrl.navigateRoot('/home');
-      },
-      error: () => this.showToast('Erro ao sair da sala.')
-    });
+    // this.classService.leaveClass(this.classId).subscribe({
+    //   next: () => {
+    //     this.showToast('Você saiu da sala.');
+    //     this.navCtrl.navigateRoot('/home');
+    //   },
+    //   error: () => this.showToast('Erro ao sair da sala.')
+    // });
   }
 
   // ============================
@@ -194,13 +212,13 @@ updatePhoto() {
   // ============================
 
   deleteClass() {
-    this.classService.deleteClass(this.classId).subscribe({
-      next: () => {
-        this.showToast('Sala excluída.');
-        this.navCtrl.navigateRoot('/home');
-      },
-      error: () => this.showToast('Erro ao excluir sala.')
-    });
+    // this.classService.deleteClass(this.classId).subscribe({
+    //   next: () => {
+    //     this.showToast('Sala excluída.');
+    //     this.navCtrl.navigateRoot('/home');
+    //   },
+    //   error: () => this.showToast('Erro ao excluir sala.')
+    // });
   }
 
   // ============================
